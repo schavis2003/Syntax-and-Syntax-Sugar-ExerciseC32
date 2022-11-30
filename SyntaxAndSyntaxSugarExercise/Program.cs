4c1fe0d47c1e7fda4c1f3a4289b8ebbd6e25f359
schavis2022 <schavis2022>
2022-11-29 23:04:25 -0500

    Syntax And Syntax Sugar Exercise Finished

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //int answer = 4; // Explicit Typing
+            //string response; // Explicit Typing
+
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else 
+            //{
+            //    response = answer + "greater than or equal to nine";
+                
+            //}
+
+            var answer = 4; // Inferred Typing
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
